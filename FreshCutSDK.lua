10779136ba01a30fbd4dc05f40621f7372277cb0
Frank Shotwell <frank@freshcut.gg>
2024-05-23 10:55:35 -0700

    Update FreshCutSDK.lua
    rm copyright text

--- FreshCutSDK.lua
+++ FreshCutSDK.lua
@@ -19,10 +19,6 @@
 --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 --@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 --
---  Copyright (C) FreshCut Interactive Inc - All Rights Reserved
---  Unauthorized copying of this file, via any medium is strictly prohibited
---  Proprietary and confidential
---
 
 -- ** DOCUMENTATION **
 --
